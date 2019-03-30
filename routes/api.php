23eecee81666037b9ed6bd9b84c55646a43be34b
taiyeoguns <taiyeoguns@yahoo.com>
2019-03-30 22:01:46 +0000

    Initial endpoints setup

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Dingo\Api\Routing\Router;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return "Laravel API";
+});
+
+$api = app(Router::class);
+
+$api->version('v1', function ($api) {
+
+    $api->get('/hey', function () {
+        return 'test';
+    });
 });
