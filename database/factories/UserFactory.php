47ff5d09663d3f4ec378aa51738f93015c21a9bf
Taiye <taiyeoguns@yahoo.com>
2019-06-01 12:26:04 +0100

    Update email for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,10 +17,10 @@ use App\Models\User;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'first_name' => $faker->firstName,
-        'last_name' => $faker->lastName,
+        'first_name' => $fn = $faker->firstName,
+        'last_name' => $ln = $faker->lastName,
         'uuid'  => Uuid::uuid4()->toString(),
-        'email' => $faker->unique()->safeEmail,
+        'email' => strtolower(sprintf('%s.%s@laravelapi.local', $fn, $ln)),
         'phone' => $faker->phoneNumber,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
